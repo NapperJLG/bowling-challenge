a1978699587475ddeaab496cdcdddcdda6d409f8
Jake Napper <napperjlg@gmail.com>
2019-05-12 19:35:14 +0100

    functions for checking if frames are spares or strikes

--- spec/frame_spec.js
+++ spec/frame_spec.js
@@ -12,6 +12,37 @@ describe('Frame', function() {
       expect(frame1.frameResult).toEqual([5,5])
     });
 
+    it('calculates frame score', function(){
+      frame1.rolls(4,5);
+      expect(frame1.frameScore()).toEqual(9);
+    });
 
+
+  });
+
+  describe('strikes and spares', function() {
+    beforeEach(function() {
+      frame1 = new Frame;
+    });
+
+    it('records frame as being a strike', function() {
+      frame1.rolls(10, 0);
+      expect(frame1.isStrike()).toEqual(true);
+    });
+
+    it('records frame as NOT being a strike', function() {
+      frame1.rolls(0, 10);
+      expect(frame1.isStrike()).toEqual(false);
+    });
+
+    it('records frame as being a spare', function() {
+      frame1.rolls(4, 6);
+      expect(frame1.isSpare()).toEqual(true);
+    });
+
+    it('records frame as NOT being a spare', function() {
+      frame1.rolls(4, 5);
+      expect(frame1.isSpare()).toEqual(false);
+    });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -4,10 +4,27 @@ var Frame = function() {
 };
 
 Frame.prototype.frameScore = function () {
+  this.score = arrSum(this.frameResult);
   return this.score;
 };
 
 Frame.prototype.rolls = function (roll1, roll2) {
   this.frameResult.push(roll1, roll2);
   this.frameCount += 1
+};
+
+Frame.prototype.isStrike = function () {
+  if(this.frameResult[0] === 10) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Frame.prototype.isSpare = function () {
+  if(this.frameScore() === 10 && this.frameResult[0] !== 10) {
+  return true;
+} else {
+  return false;
+}
 };
